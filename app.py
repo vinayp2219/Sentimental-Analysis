d016c47a0509de3bd579c4888c246e092859859b
vinay <121793602+vinayp2219@users.noreply.github.com>
2025-07-24 16:16:28 +0530

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -41,4 +42,5 @@ def home():
     return render_template("index.html", sentiment=sentiment, probabilities=probabilities)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
